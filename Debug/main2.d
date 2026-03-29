4cdb13db9995850aaa01921e18c7f0daeb1de6b1
Darshan <darshanpokhriyal2006@gmail.com>
2026-03-29 12:07:53 +0530

    Initial commit- MSP430 embedded project

---
+++ Debug/main2.d
@@ -0,0 +1,21 @@
+# FIXED
+
+main2.obj: ../main2.c
+main2.obj: C:/ti/ccs2041/ccs/ccs_base/msp430/include/msp430.h
+main2.obj: C:/ti/ccs2041/ccs/ccs_base/msp430/include/msp430g2553.h
+main2.obj: C:/ti/ccs2041/ccs/ccs_base/msp430/include/in430.h
+main2.obj: C:/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main2.obj: C:/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+
+../main2.c:
+
+C:/ti/ccs2041/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs2041/ccs/ccs_base/msp430/include/msp430g2553.h:
+
+C:/ti/ccs2041/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
